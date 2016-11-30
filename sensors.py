356147ee1d9d132d427362a867bf3a301ff340b7
Mauro Calvi <mauroc@yahoo.com>
2016-11-29 17:00:49 -0800

    added wave height calcs

--- sensors.py
+++ sensors.py
@@ -1,3 +1,4 @@
+# todo: correct with at-rest g coeffs 
 import sys
 import serial
 import math
@@ -6,39 +7,93 @@ import time
 import socket
 import os
 import math
+import collections
 from sense_hat import SenseHat
 
 GPS_IP = "127.0.0.1"
 GPS_PORT = 5005
-
 MON_IP = "127.0.0.4"
 MON_PORT = 5005
+G=9.81
+
 
 sense = SenseHat()
-f= open("log_sec.txt". "w")
-f.write("time,temperature,pressure,humidity,x_acc,y_acc,z_acc")
-x_acc = y_acc = z_acc = temperature = pressure = humidity = 0  
+#os.remove('log_sec.txt')
+f= open("log_sec.txt", "w")
+f.write("time,temperature,pressure,humidity,pitch,roll,wave ht.\r\n")
+samples = x_sq = y_sq = z_sq = temperature = pressure = humidity = log = delta_h = max_h = min_h = sum_h = sum_whts = top = bottom = 0
+prev_t = time.time()  
+d    = collections.deque([0])
+whts = collections.deque([0])
 
-while True
-    hack = time.time()
+while True:
+    time.sleep(.2)
+    t = time.time()
+    dt = t-prev_t
+    prev_t = t
     samples += 1
     
+    # read environmental values from sensor and integrate them (to calculete RMS)
     temperature += sense.get_temperature()
-    humidity    += sense.get_humidity()
     pressure    += sense.get_pressure()
-
-    z_acc += z**2
-    y_acc += y**2
-    x_acc += x**2
+    humidity    += sense.get_humidity()
     
-    if hack-log > 10
-        x_acc, y_acc, z_acc = math.sqrt(x_acc/samples), math.sqrt(y_acc/samples), math.sqrt(z_acc/samples)        
+    # read acceleration from IMU 
+    acceleration = sense.get_accelerometer_raw()
+    
+    # acceleration relative to boat's frame
+    x = acceleration['x']
+    y = acceleration['y']
+    z = acceleration['z']
+
+    # calculate squares of accel. for pitch, roll and vertical. Used to calculate RMS values  
+    x_sq += x**2
+    y_sq += y**2
+    z_sq += z**2
+    
+    # vertical non gravitational accel. relative to earth frame
+    z_vert = sqrt(x_sq+y_sq+z_sq)-1 
+    
+    # double-integrate vert. accel. (to calculate wave height)
+    delta_h += G*z_vert*dt*dt 
+    
+    # top or bottom of wave?
+    if (delta_h < d[-1] and top    == 0): 
+        top = d[-1]
+    if (delta_h > d[-1] and bottom == 0): 
+        bottom = d[-1]
+    
+    # collect wave height and calculate moving average     
+    n=10
+    if top and bottom:
+        wht =top-bottom
+        whts.append(wht)
+        drop = whts.popleft() if len(whts)>n else 0
+        sum_whts += wht-drop
+        avg_whts = sum_whts/n
+        top = bottom = 0
+    
+    # caclulate moving average of last n height samples to filter out high freq waves
+    n=10
+    d.append(delta_h)
+    drop = d.popleft() if len(d)>n else 0
+    sum_h += delta_h-drop
+    mav_h = sum_h/n
+            
+    #print(x,y,z)
+
+    if t-log > 5:
+        pitch, roll = math.sqrt(x_sq/samples), math.sqrt(y_sq/samples))        
         temperature, pressure, humidity = temperature / samples, pressure / samples, humidity/samples
         
-        f.write(str(time.time()) + ',' + str(temperature) + ',' + str(pressure)  + ',' + str(humidity) + ',' + str(x_acc) + ',' + str(y_acc) + ',' + str(z_acc)  )
-        log = hack
+        log_str = str(t)+','+str(round(temperature,3))+','+str(round(pressure,3))+','+str(round(humidity,3))+','+str(round(pitch,4))+','+str(round(roll,4))+','+str(round(avg_whts,4)) 
+
+        print(log_str)
+        f.write(log_str+"\r\n")
+	 
+        log = t
         
-        samples, x_acc = y_acc = z_acc = temperature = pressure = humidity = 0  
+        samples = x_sq = y_sq = z_sq = temperature = pressure = humidity = log = delta_h = top = bottom = 0  
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         nmea_str = "$SQPSR,"+str(temperature)+","+str(pressure)+''
         nmea_str_cs = format(reduce(operator.xor,map(ord,nmea_str),0),'X')
@@ -47,3 +102,4 @@ while True
         sock.sendto( nmea_str, (GPS_IP, GPS_PORT))
         
         
+f.close()
