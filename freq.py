48b3164f32d08b2f1fb802f0f4ca22245783ae0c
Mauro Calvi <mauroc@yahoo.com>
2016-12-07 07:54:40 -0800

    started freq domain analysis

---
+++ freq.py
@@ -0,0 +1,25 @@
+import math
+#from scipy.fftpack import fft, rfft
+from numpy import fft,array	
+
+n=100
+signal=[0]*n
+
+T=12
+amp=1
+
+for t in range(len(signal)):
+    signal[t]=amp*math.sin(2*math.pi/T*t)    
+
+spectrum=fft.fft(signal)
+
+spectrum[1].real # real component
+spectrum[1].imag # imaginary component 
+abs(spectrum[1]) # module
+
+freqs=fft.fftfreq(n)  # frequency values in spectrum
+
+for i in spectrum:
+   print(freqs[i],abs(spectrum[i])
+
+
