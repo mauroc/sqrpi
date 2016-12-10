5bda67a88aa5e574995bf49fb19c875a46816c6c
Mauro Calvi <mauroc@yahoo.com>
2016-12-10 22:25:56 +0000

    added environmental variables

--- sensors1.py
+++ sensors1.py
@@ -1,37 +1,41 @@
-# todo: correct with at-rest g coeffs 
 import sys
 import math
-#import operator
+import operator
 import time
 import collections
-from sense_hat import SenseHat
+import serial
+import socket
+import os
 
+from sense_hat import SenseHat
 from numpy import fft,array	
 import pylab as pl # sudo apt-get install python-matplotlib
 
+GPS_IP 		= "127.0.0.1"
+GPS_PORT	= 5005
+MON_IP 		= "127.0.0.4"
+MON_PORT	= 5005
+G		= 8.81
+Pi2 		= 2*math.pi
+Offset_z2 	= 0.978225246624319 # run calibrate.py to obtain this value, with sensor board resting as horizontal as possible
+Display_charts 	= False
+Window 		= 20 #secs
+Sample_rate 	= 8.0 #hz
+
+sample_period 	= 1.0/Sample_rate
+n 		= int(Window*Sample_rate) #length of the signal array
+min_wave_period = 2  #secs
+max_wave_period = 20 #secs
+min_nyq_freq 	= n/(max_wave_period*int(Sample_rate)) 
+max_nyq_freq 	= n/(min_wave_period*int(Sample_rate)) 
 
 sense = SenseHat()
 f=  open("log_sec.csv", "w")
-f.write("""time,temperature,pressure,humidity,pitch,roll,wave ht.\r\n""")
+f.write("""time,temperature,pressure,humidity,pitch,roll,wave height,wave period\r\n""")
 
-G=8.81
-Pi2 = 2*math.pi
-Offset_z2 = 0.978225246624319 # run calibrate.py to obtain this value, with sensor board resting as horizontal as possible
-
-sample_rate = 	8.0 #hz
-sample_period = 1.0/sample_rate
-
-window = 60 # secs
-n = int(window*sample_rate) #length of the signal array
 signal=[0]*n
-min_wave_period = 2 #secs
-max_wave_period = 20 #secs
-min_nyq_freq = n/(max_wave_period*int(sample_rate)) 
-max_nyq_freq = n/(min_wave_period*int(sample_rate)) 
-_signal = [2,3,2,6,3,7,5,3,6,3,4,5,6,6,5,4,4,3,3,2,4,6,7,8,9,5,7,8,4,5,6,7,8,9,0,3,4,3,4,3,2,4,4,6,8,6,]
-
 log = prev_t = t0 = time.time()  
-samples = 0
+samples = temperature = pressure = humidity = sum_x_sq = sum_y_sq = 0
 
 while True:
     time.sleep(sample_period)
@@ -39,34 +43,41 @@ while True:
     t = time.time()
     dt = t-prev_t
     prev_t = t
-    
+      
     # read acceleration from IMU 
     acceleration = sense.get_accelerometer_raw()
     
     # acceleration relative to boat's frame
     x = acceleration['x']
     y = acceleration['y']
-    z = acceleration['z']            
+    z = acceleration['z']   
+    temperature += sense.get_temperature()
+    pressure    += sense.get_pressure()
+    humidity    += sense.get_humidity()         
     #print('x,y,z values: ',round(x,4),round(y,4),round(z,4))
 
     # calc vertical non gravitational accel. relative to earth frame
     x_sq = x**2
     y_sq = y**2
     z_sq = z**2
+    sum_x_sq += x_sq
+    sum_y_sq += y_sq
+
     z_vert = math.sqrt(x_sq+y_sq+z_sq)
     vert_acc=G*(Offset_z2-z_vert)
 
     signal[samples]=vert_acc
     
     log_msg = str(samples)+', '+str(round(t-t0,4))+', '+str(round(vert_acc,4)) 
-    print(log_msg)
-    
-    f.write(log_msg+'\r\n')
+    #print(log_msg)   
+    #f.write(log_msg+'\r\n')
 
     samples += 1
 
-    if t-log > window:
-        log = t
+    if t-log > Window:
+
+	pitch, roll = math.degrees(math.asin(math.sqrt(sum_x_sq/samples))), math.degrees(math.asin(math.sqrt(sum_y_sq/samples)))
+	temperature, pressure, humidity = temperature / samples, pressure / samples, humidity/samples
 
 	# complete Fast Fourier transform of signal        
         wf=fft.fft(signal)
@@ -94,7 +105,7 @@ while True:
 		    i+=1
 
 		# period in secs of main component
-		main_period = float(n)/(float(max_index+min_nyq_freq)*sample_rate)
+		main_period = float(n)/(float(max_index+min_nyq_freq)*Sample_rate)
 		
 		#estimate average wave height for main freq component
 		estimated_wave_height = 2*amp_main_freq/((Pi2/main_period)**2) # accel amplitude / (2*Pi/T)^2 (=double sine integral constant) * 2 (=crest to trough)
@@ -104,23 +115,28 @@ while True:
 
 	print('main period: '+str(round(main_period,4))+'sec., estimated wave height: '+str(round(estimated_wave_height,4))+' mt.' )
 
-	freqs=fft.fftfreq(n)  # identify corrsesponding frequency values for x axis array (cycles/sample_units)
-        
-	freqs = [x * sample_rate for x in freqs] # convert to hertz
-        #freqs[i] = freqs[i]*sample_rate # convert to hertz
+        if Display_charts:
+		freqs=fft.fftfreq(n)  # identify corrsesponding frequency values for x axis array (cycles/sample_units)
+		
+		freqs = [x*Sample_rate for x in freqs] # convert to hertz
 
-	#pl.plot(freqs[0:n/2], spectrum[0:n/2])
-	pl.plot(freqs[min_nyq_freq:max_nyq_freq], spectrum)
-	pl.show()
-	pl.plot(signal)
-	pl.show()
-        clean_signal=fft.ifft(wf) 
-	clean_signal = [x * n/2 for x in clean_signal]
-        pl.plot(clean_signal[0:n/2])
-        pl.show()
+		pl.plot(freqs[min_nyq_freq:max_nyq_freq], spectrum)
+		pl.show()
+		pl.plot(signal)
+		pl.show()
+		clean_signal=fft.ifft(wf) 
+		clean_signal = [x * n/2 for x in clean_signal]
+		pl.plot(clean_signal[0:n/2])
+		pl.show()
 
-        samples = 0  
-        for i in range(len(signal)):
-            signal[i]=0 
-        break
+        signal = [0 for x in signal] 
+
+        log_str = str(t)+','+str(round(temperature,3))+','+str(round(pressure,3))+','+str(round(humidity,3))+','+str(round(pitch,4))+','+str(round(roll,4)) +','+str(round(estimated_wave_height,4))+','+str(round(main_period,4))  
+        print(log_str)
+        f.write(log_str+"\r\n")
+        f.flush()
+ 
+        log = t
+        samples = sum_x_sq = sum_y_sq = temperature = pressure = humidity = 0  
+
 f.close()
